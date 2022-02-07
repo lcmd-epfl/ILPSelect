1646b1f6acbaa5f65328e54e961deb8efbff6c1a
Mattuuuuh <matthieu.haeberle@epfl.ch>
2022-02-07 14:50:52 +0100

    Fixed global algorithms which double counted some norms.

--- algorithms/global_amons.py
+++ algorithms/global_amons.py
@@ -1,4 +1,5 @@
 import numpy as np 
+from numpy import linalg
 import timeit
 import gurobipy as gp
 from gurobipy import GRB
@@ -20,28 +21,26 @@ def addconstraints(Z,x):
         for G in range(maxduplicates):
             expr+=m*x[M,G]
     Z.addConstr(expr >= n)
-    Z.addConstr(expr <= 1.5*n)
+    #Z.addConstr(expr <= 1.5*n)
     return 0
 
 # objective value should then be square rooted in the end (doesn't change optimality)
 def setobjective(Z,x):
     print("Constructing objective function... ")
-    expr=gp.QuadExpr() # L2 squared distance from target rep to sum of chosen molecules reps
+    expr=gp.QuadExpr() # L2 squared distance from target rep to sum of chosen molecule reps
     T=targetdata["target_reps"][target_index]
     L=len(T) # constant size of vector representation
     
-    for i in range(L):
-        print(i, "  /  ", L)
-        expr+=T[i]**2
-        for M in database_indices:
-            CM=data[targetname+"_amons_reps"][M] # size of molecule M
-            for G in range(maxduplicates):
-                expr+=CM[i]**2 * x[M, G] - 2*T[i]*CM[i]*x[M,G]
-                for MM in database_indices:
-                    CMM=data[targetname+"_amons_reps"][MM] # size of molecule MM
-                    for GG in range(maxduplicates):
-                        expr+=CM[i]*CMM[i]*x[M,G]*x[MM,GG]
-    
+    expr+=np.linalg.norm(T)**2
+    for M in database_indices:
+        for G in range(maxduplicates):
+            CM=data[targetname+"_amons_reps"][M]
+            expr=expr-2*T.T@CM * x[M,G]
+            for MM in database_indices: 
+                for GG in range(maxduplicates):
+                    CMM=data[targetname+"_amons_reps"][MM]
+                    expr+=CM.T@CMM *x[M,G]*x[MM,GG]
+
     Z.setObjective(expr, GRB.MINIMIZE)
     print("Objective function set.")
     return 0
@@ -79,7 +78,7 @@ def main():
     # PoolSearchMode 1/2 forces to fill the solution pool. 2 finds the best solutions.
     Z.setParam("PoolSearchMode", 1) 
     # these prevent non integral values although some solutions are still duplicating -- to fix?
-    #Z.setParam("IntFeasTol", 1e-9)
+    Z.setParam("IntFeasTol", 1e-9)
     Z.setParam("IntegralityFocus", 1)
     Z.setParam("Method",1) # dual simplex method tends to keep integrality, reducing duplicate solutions. Method 0 is also possible for primal simplex.
     Z.setParam("NumericFocus",3) # computer should pay more attention to numerical errors at the cost of running time.
@@ -109,8 +108,8 @@ def main():
 target_index=0 # 0, 1, or 2 for qm9, vitc, or vitd.
 maxduplicates=2 # number of possible copies of each molecule of the database
 timelimit=3600# in seconds (not counting setup)
-numbersolutions=50 # size of solution pool
-representation=0 # 0 for SLATM, 1 for FCHL
+numbersolutions=5 # size of solution pool
+representation=1 # 0 for SLATM, 1 for FCHL
 
 # global constants
 repname=["SLATM", "FCHL"][representation]
@@ -126,5 +125,4 @@ targetname=["qm9", "vitc", "vitd"][target_index]
 size_database=len(data[targetname+"_amons_labels"]) # set this to a fixed number to take only first part of database
 #size_database=80
 database_indices=range(size_database) 
-
 main()
