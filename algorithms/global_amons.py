ece530700bf8f78ba59d207818e72a0cd8f34022
puck <puck.vangerwen@gmail.com>
2022-03-01 18:26:08 +0100

    not a lot of correlation for anything

--- algorithms/global_amons.py
+++ algorithms/global_amons.py
@@ -123,8 +123,8 @@ def main():
 target_index=0 # 0, 1, or 2 for qm9, vitc, or vitd.
 maxduplicates=2 # number of possible copies of each molecule of the database
 timelimit=3600# in seconds (not counting setup)
-numbersolutions=5 # size of solution pool
-representation=0 # 0 for SLATM, 1 for FCHL, 2 for SPAHM, 3 for CM
+numbersolutions=1000 # size of solution pool
+representation=int(sys.argv[1]) # 0 for SLATM, 1 for FCHL, 2 for SPAHM, 3 for CM
 
 # global constants
 # TODO global SOAP
