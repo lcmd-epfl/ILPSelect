057a1c1ca184c64d5dca0315354875512195ea0b
janweinreich <janweinreich286@gmail.com>
2023-09-07 09:10:11 +0200

    sml and random 4 many representations

--- sml/sml_slatm_gaussian.py
+++ sml/sml_slatm_gaussian.py
@@ -6,18 +6,22 @@ import random
 import pickle
 import pdb
 import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
 random.seed(42)
 np.random.seed(42)
 #matplotlib font size 
 plt.rcParams.update({'font.size': 18})
 
 Ha2kcal =  627.5 
+
 def krr(kernel, properties, l2reg=1e-9):
     alpha = cho_solve(kernel, properties, l2reg=l2reg)
     return alpha
 
-def get_kernel(X1, X2, charges1, charges2, sigma=1):
-    K = qml.kernels.get_local_kernel(X1, X2, charges1, charges2, sigma)
+def get_kernel(X1, X2, NA, NB, sigma=1):
+    X1 = np.concatenate(X1)
+    X2 = np.concatenate(X2)
+    K = qml.kernels.get_local_kernels_gaussian(X1, X2, NA, NB, [sigma])[0]
     return K
 
 def train_model(X_train, atoms_train, y_train, sigma=1, l2reg=1e-9):
@@ -28,51 +32,55 @@ def train_model(X_train, atoms_train, y_train, sigma=1, l2reg=1e-9):
 def train_predict_model(X_train, atoms_train, y_train, X_test, atoms_test, y_test, sigma=1, l2reg=1e-9):
     alpha_train = train_model(X_train, atoms_train, y_train, sigma=sigma, l2reg=l2reg)
 
-    K_test = get_kernel(X_train, X_test, atoms_train, atoms_test)
+    K_test = get_kernel(X_test, X_train,atoms_test, atoms_train )
     y_pred = np.dot(K_test, alpha_train)
     mae = np.abs(y_pred - y_test)[0]
     return mae, y_pred
 
-def opt_hypers(X_train, atoms_train, y_train, X_test, atoms_test, y_test):
-    sigmas = [0.5, 0.75, 1, 1.25]
-    l2regs = [1e-10, 1e-7, 1e-4]
+
+def opt_hypers(X_train, atoms_train, y_train):
+    sigmas = [0.25,0.5,0.75, 1e0, 1e1]
+    l2regs = [1e-7, 1e-6, 1e-4]
+    
+    n_folds = 5
+    kf = KFold(n_splits=n_folds)
     
     maes = np.zeros((len(sigmas), len(l2regs)))
+    
     for i, sigma in enumerate(sigmas):
         for j, l2reg in enumerate(l2regs):
-            mae, y_pred = train_predict_model(X_train, atoms_train, y_train, X_test, atoms_test,
-                                             y_test, sigma=sigma, l2reg=l2reg)
-            print('sigma', sigma, 'l2reg', l2reg, 'mae', mae)
-            maes[i,j] = mae
+            fold_maes = []
+            for train_index, val_index in kf.split(X_train):
+                X_train_fold, X_val_fold = X_train[train_index], X_train[val_index]
+                atoms_train_fold, atoms_val_fold = atoms_train[train_index], atoms_train[val_index]
+                y_train_fold, y_val_fold = y_train[train_index], y_train[val_index]
+                
+                mae, _ = train_predict_model(X_train_fold, atoms_train_fold, y_train_fold,
+                                             X_val_fold, atoms_val_fold, y_val_fold,
+                                             sigma=sigma, l2reg=l2reg)
+                fold_maes.append(mae)
+            
+            avg_mae = np.mean(fold_maes)
+            print('sigma', sigma, 'l2reg', l2reg, 'avg mae', avg_mae)
+            maes[i, j] = avg_mae
             
     min_j, min_k = np.unravel_index(np.argmin(maes, axis=None), maes.shape)
     min_sigma = sigmas[min_j]
     min_l2reg = l2regs[min_k]
-    print('min mae', maes[min_j, min_k], 'for sigma=', min_sigma, 'and l2reg=', min_l2reg)
+    print('min avg mae', maes[min_j, min_k], 'for sigma=', min_sigma, 'and l2reg=', min_l2reg)
+    
     return min_sigma, min_l2reg
 
-def get_representations(mols, params):
-    max_natoms = max([len(mol.nuclear_charges) for mol in mols])
-    elements = np.unique(np.concatenate([(mol.nuclear_charges) for mol in mols]))
 
-    reps = np.array([qml.representations.generate_fchl_acsf(mol.nuclear_charges, 
-                                                mol.coordinates,
-                                                elements=elements,
-                                                gradients=False,
-                                                pad=max_natoms)
-            for mol in mols])
-    nuclear_charges = np.array([mol.nuclear_charges for mol in mols])
-    return reps, nuclear_charges
-
-
-def get_ranking(X, X_target, Q, Q_target):
-    K = get_kernel(X, X_target, Q, Q_target, sigma=1)
-    return np.argsort(K)[::-1]
+def get_ranking(X, X_target, NA, NB):
+    
+    K = get_kernel(X, X_target, NA, NB, sigma=100)
+    return np.argsort(K.flatten())[::-1]
 
 
 if __name__ == '__main__':
 
-    NEW_FIT, PLOT = False, True
+    NEW_FIT, PLOT = True, False
     ALL_TARGETS = pd.read_csv("./targets/targets.csv")
     TARGETS_XYZ, TARGETS_y = ALL_TARGETS["xyz"].values, ALL_TARGETS["energies"].values
 
@@ -89,7 +97,7 @@ if __name__ == '__main__':
             print("Target:", xyz_target)
             target_name = xyz_target.split(".")[0]
             TARGET_PATH = f"./targets/{xyz_target}"
-            FRAGMENTS_PATH = "/home/jan/projects/molekuehl/qm7"
+            FRAGMENTS_PATH = "./qm7"
             FRAG_y = pd.read_csv(f"{FRAGMENTS_PATH}/energies_qm7.csv")
             #randomly shuffle the data
             FRAG_y = FRAG_y.sample(frac=1, random_state=42)
@@ -103,27 +111,36 @@ if __name__ == '__main__':
             for ncharge in target_mol.nuclear_charges:
                 y_target -= atom_energy_coeffs[ncharge]
 
-            X, Q = get_representations(mols, params=None)
-            qm7_nat = np.array([len(x) for x in Q])
 
-            for i, mol_ncharges in enumerate(Q):
+            nuclear_charges = np.array([mol.nuclear_charges for mol in mols])
+            mbtypes = qml.representations.get_slatm_mbtypes(nuclear_charges)
+            X = np.array([qml.representations.generate_slatm(x.coordinates, x.nuclear_charges,mbtypes=mbtypes, local=True) for x in mols])
+            N_train = np.array([len(mol.nuclear_charges) for mol in mols])
+            
+
+            for i, mol_ncharges in enumerate(nuclear_charges):
                 for ncharge in mol_ncharges:
                     y_train[i] -= atom_energy_coeffs[ncharge]
 
 
-            X_target, Q_target = get_representations([target_mol], params=None)
+            X_target = np.array([qml.representations.generate_slatm(target_mol.coordinates,
+                                                   target_mol.nuclear_charges, mbtypes=mbtypes, local=True) ])
+            N_target = [len(target_mol.nuclear_charges)]
+
             
             
-            N = [2**i for i in range(4, 13)][:7]
+            N = [2**i for i in range(4, 13)][:8]
             #N.append(len(X))
             mae_sml = []
             #SML
-            opt_ranking = get_ranking(X, X_target, Q, Q_target)[0]
+            
+            opt_ranking = get_ranking(X, X_target, N_train, N_target)
             
             for n in N:
                 ranking = opt_ranking[:n]
-                min_sigma, min_l2reg = opt_hypers(X[ranking], Q[ranking], y_train[ranking], X_target, Q_target, y_target)
-                mae, y_pred          = train_predict_model(X[ranking], Q[ranking], y_train[ranking], X_target, Q_target, y_target, sigma=min_sigma, l2reg=min_l2reg)
+                min_sigma, min_l2reg =opt_hypers(X[ranking], N_train[ranking], y_train[ranking])
+                print(min_sigma, min_l2reg)
+                mae, y_pred          = train_predict_model(X[ranking], N_train[ranking], y_train[ranking], X_target, N_target, y_target, sigma=min_sigma, l2reg=min_l2reg)
                 mae_sml.append(mae)
                 print("SML", n, mae)
 
@@ -132,33 +149,29 @@ if __name__ == '__main__':
             #five fold cross validation
             CV = 5
             all_maes_random = []
-            for i in range(CV):
+            kf = KFold(n_splits=CV, shuffle=True, random_state=130)
+
+            for i, (train_index, test_index) in enumerate(kf.split(X)):
                 maes_random = []
-                ints = np.arange(len(X))
-                print("Shuffle training data iter...",i+1,"/",CV)
-                np.random.seed(i)
-                np.random.shuffle(ints)
-                X_sub = X[ints]
-                Q_sub = Q[ints]
-                y_sub = y_train[ints]
 
                 for n in N:
 
-                    min_sigma, min_l2reg = opt_hypers(X_sub[:n], Q_sub[:n], y_sub[:n], X_target, Q_target, y_target)
-                    mae, y_pred          = train_predict_model(X_sub[:n], Q_sub[:n], y_sub[:n], X_target, Q_target, y_target, sigma=min_sigma, l2reg=min_l2reg)
+                    min_sigma, min_l2reg = opt_hypers(X[train_index][:n], N_train[train_index][:n], y_train[train_index][:n])
+                    print(min_sigma, min_l2reg)
+                    mae, y_pred          = train_predict_model(X[train_index][:n], N_train[train_index][:n], y_train[train_index][:n], X_target, N_target, y_target, sigma=min_sigma, l2reg=min_l2reg)
                     maes_random.append(mae)
                     print("Random", n, mae)
 
                 all_maes_random.append(maes_random)
 
             all_maes_random = np.array(all_maes_random)
-            np.savez(f'./results/learning_curve_sml_{target_name}.npz', train_sizes=N, all_maes_random=all_maes_random, mae_sml=mae_sml, ranking_xyz=xyzs[opt_ranking])
+            np.savez(f'./results/SLATM_learning_curve_sml_{target_name}.npz', train_sizes=N, all_maes_random=all_maes_random, mae_sml=mae_sml, ranking_xyz=xyzs[opt_ranking])
     
     if PLOT:
 
         for xyz_target in TARGETS_XYZ:
             target_name = xyz_target.split(".")[0]
-            LEARNING_CURVE = np.load(f'./results/learning_curve_sml_{target_name}.npz')
+            LEARNING_CURVE = np.load(f'./results/SLATM_learning_curve_sml_{target_name}.npz')
             MEAN_RANDOM, STD_RANDOM = np.mean(LEARNING_CURVE['all_maes_random'], axis=0)*Ha2kcal, np.std(LEARNING_CURVE['all_maes_random'], axis=0)*Ha2kcal
             SML = LEARNING_CURVE['mae_sml']*Ha2kcal
             N = LEARNING_CURVE['train_sizes']
@@ -186,4 +199,4 @@ if __name__ == '__main__':
             #grid on
             ax.grid()
             #save figure
-            fig.savefig(f'./results/learning_curve_sml_{target_name}.png', dpi=300)
+            fig.savefig(f'./results/SLATM_learning_curve_sml_{target_name}.png', dpi=300)
