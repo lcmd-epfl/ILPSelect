Average plot fix.

cluster/scripts/fps_cur_subset.py
@@ -79,11 +79,10 @@ def fps_subset(parent_folder, database, targets, representation, N, in_database=
     selector.fit(database_global_rep)
     ranking = selector.selected_idx_
 
-    SAVE_PATH = f"{parent_folder}rankings/fps_{representation}_{database}.npy"
-    np.save(SAVE_PATH, ranking[:N])
-    print(f"Saved FPS ranking of {N} fragments of database {database} to {SAVE_PATH}.")
-
     if not in_database:
+        SAVE_PATH = f"{parent_folder}rankings/fps_{representation}_{database}.npy"
+        np.save(SAVE_PATH, ranking[:N])
+        print(f"Saved FPS ranking of {N} fragments of database {database} to {SAVE_PATH}.")
         return 0
 
     for target_name in targets:

cluster/scripts/plots.py
@@ -132,28 +132,24 @@ def plots_average(parent_directory, database, targets, representation, pen, in_d
             f"{parent_directory}learning_curves/sml_{representation}_{database}_{target_name}.npz"
         )
         RANDOM_CURVE_PATH = f"{parent_directory}learning_curves/random_{representation}_{database}_{target_name}.npz"
-        if in_database:
-            FPS_CURVE_PATH = f"{parent_directory}learning_curves/fps_{representation}_{database}_{target_name}.npz"
-            CUR_CURVE_PATH = f"{parent_directory}learning_curves/cur_{representation}_{database}_{target_name}.npz"
-        else:
-            FPS_CURVE_PATH = (
-                f"{parent_directory}learning_curves/fps_{representation}_{database}.npz"
-            )
-            CUR_CURVE_PATH = (
-                f"{parent_directory}learning_curves/cur_{representation}_{database}.npz"
-            )
+        FPS_CURVE_PATH = (
+            f"{parent_directory}learning_curves/fps_{representation}_{database}_{target_name}.npz"
+        )
+        CUR_CURVE_PATH = (
+            f"{parent_directory}learning_curves/cur_{representation}_{database}_{target_name}.npz"
+        )
 
         SML_LEARNING_CURVE = np.load(SML_CURVE_PATH, allow_pickle=True)
         ALGO_CURVE = np.load(ALGO_CURVE_PATH, allow_pickle=True)
         RANDOM_CURVE = np.load(RANDOM_CURVE_PATH, allow_pickle=True)
-        #FPS_LEARNING_CURVE = np.load(FPS_CURVE_PATH, allow_pickle=True)
+        # FPS_LEARNING_CURVE = np.load(FPS_CURVE_PATH, allow_pickle=True)
         CUR_LEARNING_CURVE = np.load(CUR_CURVE_PATH, allow_pickle=True)
 
         MEAN_RANDOM.append(np.mean(RANDOM_CURVE["all_maes_random"], axis=0) * Ha2kcal)
         STD_RANDOM.append(np.std(RANDOM_CURVE["all_maes_random"], axis=0) * Ha2kcal)
         SML.append(SML_LEARNING_CURVE["mae"] * Ha2kcal)
         ALGO.append(ALGO_CURVE["mae"] * Ha2kcal)
-        #FPS.append(FPS_LEARNING_CURVE["mae"] * Ha2kcal)
+        # FPS.append(FPS_LEARNING_CURVE["mae"] * Ha2kcal)
         CUR.append(CUR_LEARNING_CURVE["mae"] * Ha2kcal)
 
     # TODO: not sure average of STDs makes sense
@@ -162,8 +158,8 @@ def plots_average(parent_directory, database, targets, representation, pen, in_d
 
     SML = np.mean(SML, axis=0)
     ALGO = np.mean(ALGO, axis=0)
-    #FPS = np.mean(SML, axis=0)
-    CUR = np.mean(ALGO, axis=0)
+    # FPS = np.mean(SML, axis=0)
+    CUR = np.mean(CUR, axis=0)
 
     fig = go.Figure()
 
@@ -183,7 +179,7 @@ def plots_average(parent_directory, database, targets, representation, pen, in_d
     fig.add_trace(go.Scatter(x=N, y=SML, name="Average SML"))
     fig.add_trace(go.Scatter(x=N, y=ALGO, name="Average frags"))
     fig.add_trace(go.Scatter(x=N, y=CUR, name="Average CUR"))
-    #fig.add_trace(go.Scatter(x=N, y=FPS, name="Average FPS"))
+    # fig.add_trace(go.Scatter(x=N, y=FPS, name="Average FPS"))
 
     fig.update_layout(
         yaxis=dict(tickmode="array", tickvals=[1, 10], type="log"),
@@ -196,4 +192,6 @@ def plots_average(parent_directory, database, targets, representation, pen, in_d
     SAVE_PATH = f"{parent_directory}plots/{representation}_{database}_average_{pen}.png"
     fig.write_image(SAVE_PATH)
     print(f"Saved plot to {SAVE_PATH}")
+
+    fig.show()
     return 0