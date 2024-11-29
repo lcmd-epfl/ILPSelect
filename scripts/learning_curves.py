84199cfc7a480df2401dc521209099c135791d35
Ksenia <ksenia.briling@epfl.ch>
2024-11-29 17:14:09 +0100

    Drugs lc sigma=1

--- scripts/learning_curves.py
+++ scripts/learning_curves.py
@@ -9,7 +9,8 @@ from qmllib.solvers import cho_solve
 
 
 #SIGMAS = [1e-2, 1e-1, 1e0, 1e1, 1e2, 1e3, 1e4]
-SIGMAS = 10.0**np.arange(-2,5.5,0.5)
+#SIGMAS = 10.0**np.arange(-2,5.5,0.5)
+SIGMAS = [1e0]
 L2REGS = [1e-7, 1e-6, 1e-4]
 
 KERNEL_CACHE = True
